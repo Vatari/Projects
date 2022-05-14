8d7c7d0ac642c9c1089510dc1199ea6b55d58254
Petar Zhelev <majestic13@gmail.com>
2022-05-14 17:06:52 +0300

    exercise

--- crossmarginCalculator.js
+++ crossmarginCalculator.js
@@ -11,6 +11,9 @@ function marginCalculator(stock, qty, buyPrice, sellPrice) {
   let total = limitSell - limitSellFee;
 
   let profit = total - limitBuy;
-  console.log(profit);
+  console.log(`Sold ${qty.toFixed(2)} -> ${stock} at 0.1% fee`);
+  console.log(`${fee.toFixed(2)} ${stock} -> deducted at 0.1% fee`);
+  console.log(`${limitSellFee.toFixed(6)} $$$ -> deducted at 0.1%`);
+  console.log(`Profit -> ${profit.toFixed(6)} $$$`);
 }
-marginCalculator("BAT", 400, 0.4016, 0.48);
+marginCalculator("BAT", 400, 0.4016, 0.4035);
